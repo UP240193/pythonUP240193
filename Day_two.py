f5e3e478455e0f3f09bd79019f8e39d53e2b53ba
UP240193 <up240193@alumnos.upa.edu.mx>
2025-06-11 11:44:48 -0600

    Dia 3

--- Day_two.py
+++ Day_two.py
@@ -22,4 +22,54 @@ print (is_light_on)
 
 
 #level 2
-print("Area de un circulo:", areaC)
+#comparar
+print(len(first_name)) > len(last_name)
+print(len(first_name)) == len(last_name)
+
+num_one = 5
+num_two = 4
+
+suma = num_one + num_two
+resta = num_one - num_two
+multiplicacion = num_one * num_two
+division = num_one / num_two
+residuo = num_one % num_two
+exp = num_one ** num_two
+divisionfloor = num_one // num_two
+
+print("Suma: ", suma)
+print("Resta :", resta)
+print("Multiplicación :", multiplicacion)
+print("División :", division)
+print("Residuo :", residuo)
+print("Exponencial : ", exp)
+print("División enterra :", divisionfloor)
+
+#Circulo
+radio = 30
+pi = 3.1416
+
+areaC = pi * radio ** 2 
+circunferencia = 2 * pi * radio 
+
+print("Area del circulo :", areaC)
+print("Circunferencia del circulo :", circunferencia)
+
+#Radio desde el imput
+radio2 = float(input("Ingresa el radio del circulo :"))
+areaC2 = pi * radio2 ** 2
+print("El area del circulo 2 es :")
+
+#Pedir datos
+nombre = input("Ingresa tu nombre :")
+apellido = input("Ingresa tu apellido :")
+pais = input("Ingresa tu país :")
+edad = int(input("Ingresa tu edad :"))
+
+print("Nombre :", nombre)
+print("Apellido :", apellido)
+print("País :", pais)
+print("Edad :", edad)
+
+#keywords de python
+help('keywords')
