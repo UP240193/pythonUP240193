470c8fab1fbf246c280100ea22916f40347eb6f9
UP240193 <up240193@alumnos.upa.edu.mx>
2025-06-19 22:56:23 -0600

    Add files via upload

---
+++ day_five.py
@@ -0,0 +1,7 @@
+#1._declara una lista vacia
+lst = list ()
+lista_vacia = []
+
+#2._ Listas usando len
+frutas = ['banana', 'naranja', 'mango', 'limón']
+vegetales = ['Tomate', 'Papa', 'Repollo','Cebolla', 'Zanahoria']
